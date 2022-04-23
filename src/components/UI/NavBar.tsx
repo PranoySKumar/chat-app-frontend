8010b720d0944671d2d02836f708904a6e679cae
Pranoy S Kumar <pranoyskumar@gmail.com>
2022-04-23 02:41:05 -0700

    added toastify for copying room id

--- src/components/UI/NavBar.tsx
+++ src/components/UI/NavBar.tsx
@@ -7,7 +7,9 @@ import { useAppDispatch } from "../../hooks/storeHooks";
 import { chatActions } from "../../stores/slices/chatSlice";
 import { messageActions } from "../../stores/slices/messagesSlice";
 import { uiActions } from "../../stores/slices/uiSlice";
+import { ToastContainer, toast } from "react-toastify";
 import { socket } from "../../utils/socket-io";
+import "react-toastify/dist/ReactToastify.css";
 
 const icon_color = "#9400d3";
 export default function NavBar() {
@@ -27,31 +29,42 @@ export default function NavBar() {
   const copyToClipBoard = async () => {
     const room_id = localStorage.getItem("ROOM_ID");
     await navigator.clipboard.writeText(room_id!);
-    alert("Copied");
+    toast.success("🦄 copied!", {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
   };
   return (
-    <AppBar
-      sx={{
-        position: "fixed",
-        width: { lg: "51.4rem", md: "55.2rem", sx: "5rem" },
-        "&.MuiPaper-root": { backgroundColor: "white" },
-        zIndex: 1,
-      }}
-      position={"static"}
-    >
-      <Toolbar sx={{ color: "#9400d3", justifyContent: "space-between" }}>
-        <IconButton onClick={onClickHandler} sx={{ color: icon_color, display: { lg: "none" } }}>
-          <Menu />
-        </IconButton>
-        <Button onClick={copyToClipBoard} variant="text" sx={{ color: "#9400d3" }}>
-          <Typography>Meet Up</Typography>
-          <ContentCopy />
-        </Button>
+    <>
+      <ToastContainer />
+      <AppBar
+        sx={{
+          position: "fixed",
+          width: { lg: "51.4rem", md: "55.2rem", sx: "5rem" },
+          "&.MuiPaper-root": { backgroundColor: "white" },
+          zIndex: 1,
+        }}
+        position={"static"}
+      >
+        <Toolbar sx={{ color: "#9400d3", justifyContent: "space-between" }}>
+          <IconButton onClick={onClickHandler} sx={{ color: icon_color, display: { lg: "none" } }}>
+            <Menu />
+          </IconButton>
+          <Button onClick={copyToClipBoard} variant="text" sx={{ color: "#9400d3" }}>
+            <Typography>Meet Up</Typography>
+            <ContentCopy />
+          </Button>
 
-        <IconButton onClick={onLogOutHandler}>
-          <LogoutIcon sx={{ color: icon_color }} />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+          <IconButton onClick={onLogOutHandler}>
+            <LogoutIcon sx={{ color: icon_color }} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 }
